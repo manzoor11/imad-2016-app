228262d45402f4e7df6b5b16d3fb83d48be3d9c3
manzoor11 <manzoor11@gmail.com>
2016-10-01 01:55:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ function createTemplate(data)
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
-    var content = data.content
+    var content = data.content;
 
         var htmlTemplate = `
             <html>
