6074c3bbee8523bf22d1228ad24f377140b8d48c
manzoor11 <manzoor11@gmail.com>
2016-10-02 02:09:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,10 +98,10 @@ app.get('/', function (req, res) {
 app.get(':/articleName', function(req, res)
 {    // articleName = article-one
     //articles[articleName] = {} content object for article one
-    articleName =req.params.articleName;
+   var articleName =req.params.articleName;
     res.send(createTemplate(articles[articleName]));  
 });
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
