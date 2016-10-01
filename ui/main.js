3834fae5b684fad1877a299aa30d3187275c58ab
manzoor11 <manzoor11@gmail.com>
2016-10-02 03:05:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = 'New value';
 
 // move the image
 var img = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft +10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick =function() {
-    img.style.marginleft   = '120px';
+    var interval = setInterval(moveRight,100);
+    img.style.marginLeft   = '120px';
 };
