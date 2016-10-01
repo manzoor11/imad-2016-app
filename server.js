97c7227c1822f309413ff02f80c4398d91b8abd0
manzoor11 <manzoor11@gmail.com>
2016-10-02 02:31:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ function createTemplate(data)
             <html>
             <head>
                 <title>
-                    Artice One |Manzoor
+                    ${title}
                 </title>  
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                    <link href="/ui/style.css" rel="stylesheet" />
