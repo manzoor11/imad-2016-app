92d7b13cbf2ef16d1af405594f7ce9670693747b
manzoor11 <manzoor11@gmail.com>
2016-10-02 13:37:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,20 @@ button.onclick = function() {
     request.open('GET', 'http://manzoor11.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//submit Name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getelementById('submit_btn');
+submit.onclick = function() {
+  // make a request to server
+  
+  //capture a list of names and render it as a list
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for (var i=0;i<names.length;i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHtml = list;
+};
