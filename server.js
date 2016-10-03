ae4a493673a6b2517d895967bb259deebdc3b267
manzoor11 <manzoor11@gmail.com>
2016-10-04 01:22:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,16 +11,6 @@ app.get('/counter', function(req, res) {
     res.send(counter.toString());
 });
 
-var names=[];
-app.get('/submit-name/:name', function(req, res) {  // URL: /submit-name?name=manz if we give query instead of params
- // get the name 
- var name = req.params.name;
- 
- names.push(name);
- // JSON : java s
- res.send(JSON.stringify(names));
-});
-
 var articles = {
                  'article-one': {
                     title: 'Article | Manzoor',
@@ -130,6 +120,15 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names=[];
+app.get('/submit-name/:name', function(req, res) {  // URL: /submit-name?name=manz if we give query instead of params
+ // get the name 
+ var name = req.params.name;
+ 
+ names.push(name);
+ // JSON : java s
+ res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
