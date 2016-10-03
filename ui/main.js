18d10a3f7ee7849b27e5e4583a618879b587c1c6
manzoor11 <manzoor11@gmail.com>
2016-10-04 01:17:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick = function() {
      // not done yet
   };
     // make request
-    request.open('GET', 'http://manzoor11.imad.hasura-app.io/submit-name/' + name, true);
+    request.open('GET', 'http://manzoor11.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
@@ -34,15 +34,32 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
-  // make a request to server
-  
-  //capture a list of names and render it as a list
-  var names = request.responseText;
-  names = JSON.parse(names);
-  var list = '';
-  for (var i=0;i<names.length;i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+    
+    // creat a request to counter end point
+     var request = new XMLHttpRequest();
+     
+         //capture the response
+      request.onreadystatechange = function () {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            // take some action 
+            if(request.status === 200) {
+
+              //capture a list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0;i<names.length;i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+            }
+        }
+        // Not done yet
+      };
+      
+    // make request
+    request.open('GET', 'http://manzoor11.imad.hasura-app.io/submit-name/' + name, true);
+    request.send(null);
+    
 };
